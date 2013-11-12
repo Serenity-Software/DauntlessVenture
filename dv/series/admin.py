f50d7b70e31429e68c22339c7af17ef081d06250
Ben Reece <benreece@gmail.com>
2013-11-12 01:56:28 -0500

    Updates

--- dv/series/admin.py
+++ dv/series/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from dv.series.models import Series
+from dv.series.models import Season
+from dv.series.models import Episode
+from dv.series.models import Act
+from dv.series.models import Scene
+from dv.series.models import Component
+from dv.series.models import ComponentType
 
-# Register your models here.
+admin.site.register(Series)
+admin.site.register(Season)
+admin.site.register(Episode)
+admin.site.register(Act)
+admin.site.register(Scene)
+admin.site.register(Component)
+admin.site.register(ComponentType)
